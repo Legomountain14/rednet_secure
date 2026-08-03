4f096d1339e9284cdaeddeadf0a9cd4de806ff30
legomountain14 <legomountain14@gmail.com>
2026-08-03 12:38:31 -0500

    first commit

---
+++ randutils.lua
@@ -0,0 +1,14 @@
+local charset = {}  do -- [0-9a-zA-Z]
+    for c = 48, 57  do table.insert(charset, string.char(c)) end
+    for c = 65, 90  do table.insert(charset, string.char(c)) end
+    for c = 97, 122 do table.insert(charset, string.char(c)) end
+end
+
+local function randomString(length)
+    if not length or length <= 0 then return '' end
+    math.randomseed(os.time('ingame')^5)
+    return randomString(length - 1) .. charset[math.random(1, #charset)]
+end
+
+local randutils = {randomString = randomString}
+return randutils
